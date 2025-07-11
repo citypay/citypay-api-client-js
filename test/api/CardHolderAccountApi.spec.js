2170f9c759cc9a04077befa24237b3fe3ad99778
oxperience <a.afonso95@icloud.com>
2025-07-11 12:03:32 +0100

    Refactor CardHolderAccountApi test file to prepare for the next iteration of CardHolderAccountApi tests.

--- test/api/CardHolderAccountApi.spec.js
+++ test/api/CardHolderAccountApi.spec.js
@@ -1,5 +1,3 @@
-const {v4: uuidv4} = require('uuid');
-
 /**
  * CityPay Payment API
  *  This CityPay API is a HTTP RESTful payment API used for direct server to server transactional processing. It provides a number of payment mechanisms including: Internet, MOTO, Continuous Authority transaction processing, 3-D Secure decision handling using RFA Secure, Authorisation, Refunding, Pre-Authorisation, Cancellation/Voids and Completion processing. The API is also capable of tokinsed payments using Card Holder Accounts.  ## Compliance and Security <aside class=\"notice\">   Before we begin a reminder that your application will need to adhere to PCI-DSS standards to operate safely   and to meet requirements set out by Visa and MasterCard and the PCI Security Standards Council including: </aside>  * Data must be collected using TLS version 1.2 using [strong cryptography](#enabled-tls-ciphers). We will not accept calls to our API at   lower grade encryption levels. We regularly scan our TLS endpoints for vulnerabilities and perform TLS assessments   as part of our compliance program. * The application must not store sensitive card holder data (CHD) such as the card security code (CSC) or   primary access number (PAN) * The application must not display the full card number on receipts, it is recommended to mask the PAN   and show the last 4 digits. The API will return this for you for ease of receipt creation * If you are developing a website, you will be required to perform regular scans on the network where you host the   application to meet your compliance obligations * You will be required to be PCI Compliant and the application must adhere to the security standard. Further information   is available from [https://www.pcisecuritystandards.org/](https://www.pcisecuritystandards.org/) * The API verifies that the request is for a valid account and originates from a trusted source using the remote IP   address. Our application firewalls analyse data that may be an attempt to break a large number of security common   security vulnerabilities.
@@ -26,11 +24,7 @@ const {v4: uuidv4} = require('uuid');
 }(this, function (expect, citypay_api_client) {
     'use strict';
 
-    const client_id = process.env.CP_CLIENT_ID;
-    const licence_key = process.env.CP_LICENCE_KEY;
-    const merchant_id = process.env.CP_MERCHANT_ID;
-
-    let instance;
+    var instance;
 
     beforeEach(function () {
         instance = new citypay_api_client.CardHolderAccountApi();
@@ -56,10 +50,10 @@ const {v4: uuidv4} = require('uuid');
         describe('accountCardDeleteRequest', function () {
             it('should call accountCardDeleteRequest successfully', function (done) {
                 //uncomment below and update the code to test accountCardDeleteRequest
-                instance.accountCardDeleteRequest(function (error) {
-                    if (error) throw error;
-                    expect().to.be();
-                });
+                //instance.accountCardDeleteRequest(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
                 done();
             });
         });
@@ -94,33 +88,14 @@ const {v4: uuidv4} = require('uuid');
             });
         });
         describe('accountCreate', function () {
-
-            const cha_id = uuidv4();
-
-            it('should call accountCreate successfully', async () => {
-
-
-                let api = new citypay_api_client.CardHolderAccountApi(instance);
-                let ac = new citypay_api_client.AccountCreate(cha_id);
-
-                ac.contact = new citypay_api_client.ContactDetails.constructFromObject({
-                    address1: "Mielles House,  Rue des Mielles",
-                    area: "St Helier",
-                    company: "CityPay Limited",
-                    country: "JE",
-                    email: "dev@citypay.com",
-                    firstname: "Integration",
-                    lastname: "Test",
-                    postcode: "JE2 3QD"
-                });
-
-                const result = await api.accountCreate(ac);
-
-                expect(result.account_id).to.equal(cha_id);
-                expect(result.contact.address1).to.equal("Mielles House,  Rue des Mielles");
-
+            it('should call accountCreate successfully', function (done) {
+                //uncomment below and update the code to test accountCreate
+                //instance.accountCreate(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
             });
-
         });
         describe('accountDeleteRequest', function () {
             it('should call accountDeleteRequest successfully', function (done) {
